dcc44c191d4ec175de9d0e515bf1b30851b30585
Legend Paul <qamunyamaina950@gmail.com>
2025-09-30 13:49:30 +0300

    Validate form before submission

--- controller/handleFormSubmission.js
+++ controller/handleFormSubmission.js
@@ -1,9 +1,24 @@
 const messages = require("../data");
+const { body, validationResult } = require("express-validator");
 
-function handleFormSubmission(req, res) {
-    const { user, text } = req.body;
-    messages.push({ user, text, added: new Date() });
-    res.status(201).redirect("/");
-}
+const message = "must contain aleast 2 letter";
+const validate = [
+    body("user").trim().isLength({ min: 2 }).withMessage(`User ${message}`),
+    body("text").trim().isLength({ min: 2 }).withMessage(`Text ${message}`),
+];
+
+const handleFormSubmission = [
+    validate,
+    (req, res) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("new", { errors: errors.array() });
+        }
+        const { user, text } = req.body;
+        messages.push({ user, text, added: new Date() });
+        res.status(201).redirect("/");
+    },
+];
 
 module.exports = handleFormSubmission;
